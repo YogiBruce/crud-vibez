342db9b776294a36d6573d2bc03abb2b552a46b5
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-13 18:00:26 -0800

    User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+// const Thought = require('./Thought');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
