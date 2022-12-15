a4c01f330bae232d91d390bf88f82bfeb89330d5
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-14 17:14:53 -0800

    api/user-routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+//Import user-controller functions
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  addFriend,
+  deleteUser,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+// User routes
+router
+    .route("/")
+    .get(getAllUsers)
+    .post(createUser);
+
+// User :id routes
+router
+    .route("/:id")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// User :id friends :id routes
+router
+    .route('/:id/friends/:friendId')
+    .put(addFriend)
+    .delete(deleteFriend)
+
+
+module.exports = router
