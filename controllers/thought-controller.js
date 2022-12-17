0433dff9e1a82cde26b6d6aec8f5afd7f30ccfdb
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-17 11:00:21 -0800

    git all thoughts controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,18 +2,33 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
     //getAllThoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .sort({ _id: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 
     //getThoughtById
 
+
     //createThought
+
 
     //updateThought
 
+
     //addReaction
+
 
     //deleteThought
 
+
     //deleteReaction
+
 
 };
 
