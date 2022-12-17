c18bb798c6efab0150efb9caecb7241545b21df1
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-17 10:09:46 -0800

    createUser controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,7 +39,11 @@ const userController = {
   },
 
   //createUser
-
+ createUser({ body }, res){
+    User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err))
+ }
   //updateUser
 
   //deleteUser
