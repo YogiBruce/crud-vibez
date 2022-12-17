d6780f4440822033079138f3baa06567bcf43096
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-17 10:02:31 -0800

    getUserById

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,6 +14,29 @@ const userController = {
   },
 
   //getUserById
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "__v",
+      })
+      .populate({
+        path: "friends",
+        select: "__v",
+      })
+      .select("__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
   //createUser
 
