c33442f9511e0ea99475271c5b066a0aeebb6e85
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-17 09:50:30 -0800

    getAllUsers and Thought debug

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,29 @@
 const { User } = require("../models");
 
 const userController = {
-  
-    //getAllUsers
-  
-    //getUserById
-  
-    //createUser
- 
-    //updateUser
- 
-    //deleteUser
-  
-    //addFriend
-  
-    //deleteFriend
+  //getAllUsers
+  getAllUsers(req, res) {
+    User.find({})
+      .select("__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  //getUserById
+
+  //createUser
+
+  //updateUser
+
+  //deleteUser
+
+  //addFriend
+
+  //deleteFriend
 };
 
 module.exports = userController;
